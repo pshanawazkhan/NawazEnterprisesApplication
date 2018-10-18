a4346a1f86b472a2ed878a5735dddbb33ce9b547
SHANAWAZ KHAN <SHANAWAZ KHAN@192.168.0.3>
2018-10-18 12:13:53 +0530

    1st commit from repository

--- NawazEnterprisers/src/main/java/com/nawaz/main/NawazEnterprisers/NawazEnterprisersApplication.java
+++ NawazEnterprisers/src/main/java/com/nawaz/main/NawazEnterprisers/NawazEnterprisersApplication.java
@@ -8,5 +8,6 @@ public class NawazEnterprisersApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NawazEnterprisersApplication.class, args);
+		System.out.println("---------------");
 	}
 }
